5c14e7f940edbb59801c76778f1a49846d7d32eb
Alex Baumgartner <aabaumgartner@gmail.com>
2024-01-28 10:20:30 -0800

    added weather icons, all html is built with placeholders for weather information, added a bit more to the CSS including weather icon properties, and began working on variables for API key and URL in JS

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,22 @@
 var apiKey = "b1d28790f4ccf5e498609e27f6817a7e";
-var apiURL = "https://api.openweathermap.org/data/2.5/forecast?q=London&appid=";
+var apiURL = "https://api.openweathermap.org/data/2.5/forecast?q=london";
+
+// forecast cards
+var forecstCard =
+  '<div class="forecast-cards">\
+         <h3>(1/24/2024)</h3>\
+         <img src="./assets/images/sun.png" alt="sunny icon" class="weather-icon">\
+          <p>Temp</p>\
+          <p>Wind</p>\
+          <p>Humidity</p>\
+   </div>';
+
+function createForecastCards() {
+  console.log("creating forecast cards");
+
+  for (let i = 0; i < 5; i++) {
+    $(".forecast-container").append(forecstCard);
+  }
+}
+
+createForecastCards();

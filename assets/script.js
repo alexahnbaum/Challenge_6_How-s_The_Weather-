174bc29bbfce31b1201b57ddfb0ca1a2b50e5f92
Alex Baumgartner <aabaumgartner@gmail.com>
2024-01-30 06:03:50 -0800

    added some ids in HTML for my JS to select, and adding some JS - handler on click search buton, and fixed the issue with my fetch request working with the URL and API key

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,31 @@
-var apiKey = "b1d28790f4ccf5e498609e27f6817a7e";
-var apiURL = "https://api.openweathermap.org/data/2.5/forecast?q=london";
+// API key and API URL with city instead of coordinates
 
-// forecast cards
+var apiKey = "b1d28790f4ccf5e498609e27f6817a7e";
+var apiURL = "https://api.openweathermap.org/data/2.5/forecast?q=";
+
+// Button Element
+var buttonEl = $(".button");
+// var city = userInputEl.value;
+
+// Take user input from city-search element and store it in a variable to use in the fetch request to the API
+buttonEl.on("click", function () {
+  var userInput = $("#city-search").val();
+
+  console.log(userInput);
+
+  fetch(apiURL + userInput + "&appid=" + apiKey)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      $("#today-city-date").append(data.city.name, data.city.country);
+    });
+});
+
+// Append data to be displayed on website
+
+// Forecast cards
 var forecstCard =
   '<div class="forecast-cards">\
          <h3>(1/24/2024)</h3>\

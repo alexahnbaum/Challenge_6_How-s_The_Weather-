4e2f60d9c99133ee98de7d8bcbec83c7aefd8a9e
Alex Baumgartner <aabaumgartner@gmail.com>
2024-02-02 06:13:01 -0800

    reached MVP, clickign on previously searched cities displays weather and forecast, small changes to CSS and tested app a bunch

--- assets/script.js
+++ assets/script.js
@@ -8,20 +8,32 @@ var forecastApiUrl = "https://api.openweathermap.org/data/2.5/forecast?";
 // Button Element
 var buttonEl = $("#button");
 
-// Take user input from city-search element and store it in a variable to use in the fetch request to the API
-
-// 2 functions:
-// - 1 function for button click: show current weather
-// - 1 function for button click: show 5-day forecast
-
-buttonEl.on("click", function (firstClick) {
-  var weatherAndForecast = firstClick;
-
+// Initial search button click
+buttonEl.on("click", function () {
   var userInput = $("#city-search").val();
 
+  // Call function getWeather and pass userInput value into it
+  getWeather(userInput);
   console.log(userInput);
+  // Append searched city to UL below search bar as a list
+  var greyButton = "<button id=grey-button>" + userInput + "</button>";
 
-  fetch(weatherApiUrl + userInput + "&appid=" + apiKey + "&units=imperial")
+  $("#city-list").append(greyButton);
+
+  // Listen for clicks on grey buttons - buttons with searched preiously cities
+  $("#city-list").on("click", "button", function () {
+    var cityClicked = $(this).text();
+    console.log(cityClicked);
+    // Call function getWeather and pass cityClicked text into it
+    getWeather(cityClicked);
+  });
+});
+
+// Function getWeather to do 2 fetches:
+// - 1 fecth to the API to show current weather
+// - 1 fecth to the API to show five day forecast
+function getWeather(cityName) {
+  fetch(weatherApiUrl + cityName + "&appid=" + apiKey + "&units=imperial")
     .then(function (response) {
       return response.json();
     })
@@ -29,13 +41,13 @@ buttonEl.on("click", function (firstClick) {
       console.log(data);
 
       $("#today-city-date").text(
-        `${data.name} ${dayjs.unix(data.dt).format("DD MM, YYYY")}`
+        `${data.name} (${dayjs.unix(data.dt).format("MM DD, YYYY")})`
       );
+      var iconUrl = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+      $("#icon").attr("src", iconUrl);
       $("#temp").text(`${data.main.temp} F`);
       $("#wind").text(`${data.wind.speed} MPH`);
       $("#humidity").text(`${data.main.humidity} %`);
-      var iconUrl = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-      $("#icon").attr("src", iconUrl);
 
       // fetch forecast API
       fetch(
@@ -61,27 +73,16 @@ buttonEl.on("click", function (firstClick) {
           for (let i = 3; i < data.list.length; i += 8) {
             var forecastIconUrl = `https://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png`;
             var forecastCard = `<div class="forecast-cards">\
-            <h3>${dayjs.unix(data.list[i].dt).format("DD MM, YYYY")}</h3>\
+            <h3>${dayjs
+              .unix(data.list[i].dt)
+              .format("MM DD, YYYY")} <img src= '${forecastIconUrl}'/></h3>\
             <p>Temp ${data.list[i].main.temp} F</p>\
             <p>Wind ${data.list[i].wind.speed} MPH</p>\
             <p>Humidity ${data.list[i].main.humidity} %</p>\
-            <img src= '${forecastIconUrl}'/>
+            
           </div>`;
             $(".forecast-container").append(forecastCard);
           }
         });
     });
-
-  // Append searched city to UL below search bar as a list
-  var greyButton = "<button id=grey-button>" + userInput + "</button>";
-
-  $("#city-list").append(greyButton);
-
-  console.log(weatherAndForecast);
-
-  // Listen for clicks on grey buttons - buttons with searched preiously cities
-  // greyButton.on("click", function () {
-  //   weatherAndForecast;
-  // });
-  // console.log(weatherAndForecast);
-});
+}
